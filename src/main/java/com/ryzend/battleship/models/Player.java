6f21a911a523ef5434af533148ecacfdad5b2fe8
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-09-30 11:14:22 +0400

    feat: Add Player class

---
+++ src/main/java/com/ryzend/battleship/models/Player.java
@@ -0,0 +1,13 @@
+package com.ryzend.battleship.models;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class Player {
+
+    private final String name;
+}
